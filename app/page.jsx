651ce0574ce5e79607389477b5ead2778ed0ad83
Baut Antoine <bauta@classes.local>
2023-12-12 09:32:02 +0100

    link

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
+"use client"
+import { useEffect } from "react"
+
 export default function HomePage() {
+    /*useEffect(() => {
+        window.alert("Bienvenue sur mon site")
+    })*/
+
+    console.log("HomePage [Reading]")
+
     return (
         <>
             <h1>My Games</h1>
             <p>
-                Only the best gales, reviewed for you.
+                Only the best games, reviewed for you.
             </p>
         
         </>

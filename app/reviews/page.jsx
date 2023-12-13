86173bc576acd4130407fc05d1e7ae8c83e37a89
BoubouRSCL <abaut2001@gmail.com>
2023-12-13 11:52:32 +0100

    rajout

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,28 +1,26 @@
 import Link from "next/link"
 import Heading from "@/components/Heading"
+import { getReviews } from "@/lib/review"
 
-export default function ReviewsPage()
+export default async function ReviewsPage()
 {
+    const reviews = await getReviews()
+
     return (
         <>
             <Heading>Reviews</Heading>
             <p>Here we'll list all the reviews</p>
-            <nav>
-                <ul className="flex flex-col gap-3">
-                    <li className="border rounded shadow hover:shadow-x1 w-80 bg-slate-50">
-                        <Link href='/reviews/diablo'>
-                            <img src="/images/diablo.jpg" alt="image de diablo" className="rounded-t" />
-                            <h2 className="font-orbitron font-semidbold py-1 text-center">Diablo 4</h2>
+            <ul className='flex flex-col gap-3'>
+                {reviews.map((review) => (
+
+                    <li key={review.slug} className='border rounded shadow hover:shadow-xl w-80 bg-slate-50'>
+                        <Link href={`/reviews/${review.slug}`}>
+                            <img src={review.image} alt={`ìmage de ${review.title}`} className='rounded-t' />
+                            <h2 className='font-orbitron font-semibold py-1 text-center'>{review.title}</h2>
                         </Link>
-                        </li>
-                    <li className="border rounded shadow hover:shadow-x1 w-80 bg-slate-50">
-                        <Link href='/reviews/spider-man'>
-                            <img src="/images/spiderman2.jpg" alt="image de spiderman" className="rounded-t" />
-                            <h2 className="font-orbitron font-semidbold py-1 text-center">Spiderman 2</h2>
-                        </Link>
-                        </li>
-                </ul>
-            </nav>
+                    </li>
+                ))}
+            </ul>
             
         </>
     )

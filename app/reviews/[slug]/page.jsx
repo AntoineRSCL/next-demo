4790795a467e540ad5c3840feeae1f1876993f02
BoubouRSCL <abaut2001@gmail.com>
2023-12-18 11:30:54 +0100

    Rajout link

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,9 +1,17 @@
 import Heading from "@/components/Heading"
+import ShareButtons from "@/components/ShareButtons"
 import { getReview, getSlugs } from "@/lib/review"
 
 export async function generateStaticParams() {
     const slugs = await getSlugs()
     return slugs.map((slug) => ({slug}))
+}
+
+export async function generateMetadata(props) {
+    const review = await getReview(props.params.slug)
+    return {
+        title: review.title
+    }
 }
 
 export default async function ReviewPage({params: {slug}})
@@ -13,7 +21,10 @@ export default async function ReviewPage({params: {slug}})
     return (
         <>
             <Heading>{review.title}</Heading>
-            <p className="italic pb-2">{review.date}</p>
+            <div className="flex gap-3 items-baseline">
+                <p className="italic pb-2">{review.date}</p>
+                <ShareButtons />
+            </div>
             <img src={review.image} alt={`image de ${review.title}`} className='mb-4 rounded w-screen mx-auto' />
             <article dangerouslySetInnerHTML={{__html: review.body}} className="prose lg:prose-xl"></article>    
         </>
---
+++ components/ShareLinkButton.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { LinkIcon } from '@heroicons/react/24/outline'
+
+
+export default function ShareLinkButton(){
+    const [clicked, setClicked] = useState(false)
+    const handleClick = () => {
+        navigator.clipboard.writeText(window.location.href)
+        setClicked(true)
+        setTimeout(() => setClicked(false),1500)
+    }
+
+    return (
+        <button onClick={handleClick} className=' flex gap-1 border px-2 py-1 my-3 rounded text-slate-500 text-sm hover:bg-orange-300 hover:text-slate-700'>
+            <LinkIcon className='h-4 w-4'/>
+            {clicked ? 'Link copied' : 'Share link'}
+        </button>
+    )
+
+
+
+
+}

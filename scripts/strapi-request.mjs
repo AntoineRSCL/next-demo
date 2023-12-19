0dc45d754611aced0b3cda58adf93800d17486f9
Baut Antoine <bauta@classes.local>
2023-12-19 11:26:17 +0100

    qs pour requete complexe sur strapi

---
+++ scripts/strapi-request.mjs
@@ -0,0 +1,18 @@
+import { writeFileSync } from 'node:fs'
+import qs from 'qs'
+
+//const url = "http://localhost:1337/api/reviews" + '?populate=*'
+const url = "http://localhost:1337/api/reviews" + '?' + qs.stringify({
+    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    populate: {image: {fields: ['url']}},
+    sort: ['publishedAt'],
+    pagination: { pageSize: 6 }
+},{encodeValuesOnly : true})
+const response = await fetch(url)
+const body = await response.json()
+console.log(body)
+console.log(JSON.stringify(body,null,2))
+const formatted = JSON.stringify(body,null,2)
+const file = 'scripts/strapi-response.json'
+
+writeFileSync(file, formatted, 'utf-8')

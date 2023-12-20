596e71ee7295fb4499e0e9255396ab11acc12309
Baut Antoine <bauta@classes.local>
2023-12-20 10:38:33 +0100

    generateParamStatic

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -3,10 +3,11 @@ import qs from 'qs'
 
 //const url = "http://localhost:1337/api/reviews" + '?populate=*'
 const url = "http://localhost:1337/api/reviews" + '?' + qs.stringify({
-    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    filters: {slug: {$eq: 'spiderman-2'}},
+    fields: ['slug', 'title', 'subtitle', 'publishedAt', 'body'],
     populate: {image: {fields: ['url']}},
-    sort: ['publishedAt'],
-    pagination: { pageSize: 6 }
+    sort: ['publishedAt:desc'],
+    pagination: { pageSize: 1, withCount: false }
 },{encodeValuesOnly : true})
 const response = await fetch(url)
 const body = await response.json()
